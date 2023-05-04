748796e871d9327747cb13859d96c617d04dc9a8
Tejas <teja.bhandvalkar@gmail.com>
2023-05-04 17:50:07 +0530

    Email

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function sendmail(params){
+function sendmail(params) {
     var tempParams = {
-        name:document.getElementById("Name").value,
-        email:document.getElementById("Email").value,
-        message:document.getElementById("Message").value,
+        from_name: document.getElementById("Name").value,
+        email_id: document.getElementById("Email").value,
+        message: document.getElementById("Message").value,
     };
 
-    emailjs.send('service_y6rudtd','template_dau378q',tempParams).then(function(res){
-        console.log("Success!!!",res.status);
+    emailjs.send('service_y6rudtd', 'template_dau378q', tempParams).then(function (res) {
+        console.log("Success!!!", res.status);
     })
 }
